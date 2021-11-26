43e6c6af813f802188215e8b3acb1286755667e8
Henry Reveiller-Guillon <72794513+henry-reveillerguillon@users.noreply.github.com>
2021-11-26 12:00:08 +0100

    ✨ Add routes

--- src/Covoit/Covoit.jsx
+++ src/Covoit/Covoit.jsx
@@ -1,16 +1,18 @@
-import "./Covoit.scss"
-
-import Register from "./Register/Register"
-import Login from "./Login/Login"
-import Ads from "./Ads/Ads"
+import './Covoit.scss'
+import { Link, Routes, Route } from 'react-router-dom'
+import Register from './Register/Register'
+import Login from './Login/Login'
+import Ads from './Ads/Ads'
 
 export default function Covoit() {
   return (
     <>
-      <h1>En voiture Michel !</h1>
-      <Register />
-      <Login />
-      <Ads />
+      <h1>En voiture Michelle !</h1>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/ads" element={<Ads/>} />
+      </Routes>
     </>
   )
 }
