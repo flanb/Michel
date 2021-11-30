380ff24e1340a87d3b1bb3725e48a004c04b8176
Henry Reveiller-Guillon <72794513+henry-reveillerguillon@users.noreply.github.com>
2021-11-30 14:40:27 +0100

    💄 Michel se fait une beauté

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter } from "react-router-dom"
-import { CookiesProvider } from "react-cookie"
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CookiesProvider>
         <App />
-      </CookiesProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
