2c301fa758b4aca9edda64355136ce31e5b3ca61
Henry Reveiller-Guillon <72794513+henry-reveillerguillon@users.noreply.github.com>
2021-12-02 12:24:12 +0100

    💄 Style inscription

--- src/User/Register/Register.jsx
+++ src/User/Register/Register.jsx
@@ -62,6 +62,25 @@ export default function Register() {
               ref={passInput}
             />
             <label htmlFor="password">Mot de passe*</label>
+            <svg
+              width="24"
+              height="18"
+              viewBox="0 0 24 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              onClick={() => {
+                passInput.current.type === 'password'
+                  ? (passInput.current.type = 'text')
+                  : (passInput.current.type = 'password')
+              }}
+            >
+              <path d="M1 9C1 9 5 1 12 1C19 1 23 9 23 9C23 9 19 17 12 17C5 17 1 9 1 9Z" />
+              <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" />
+            </svg>
           </div>
           <span ref={response}>Les champs marqués * sont obligatoires</span>
           <Btn type="submit">Je m'inscris</Btn>
