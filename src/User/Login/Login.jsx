d28dc6cace1efbb32f1cb026b34c2a74021eb787
Henry Reveiller-Guillon <72794513+henry-reveillerguillon@users.noreply.github.com>
2021-12-02 12:21:57 +0100

    💄 style login

--- src/User/Login/Login.jsx
+++ src/User/Login/Login.jsx
@@ -1,10 +1,10 @@
-import "./Login.scss"
+import './Login.scss'
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
-import { useRef } from "react"
-import { useNavigate } from "react-router-dom"
-import Btn from "../../Btn/Btn"
-import { Link } from "react-router-dom"
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Btn from '../../Btn/Btn'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   const emailInput = useRef(null)
@@ -18,19 +18,19 @@ export default function Login() {
       .then((userCredential) => {
         const user = userCredential.user
         response.current.innerHTML = `Connecté à ${user.email}`
-        navigate("/user")
+        navigate('/user')
       })
       .catch((error) => {
-        if (error.code === "auth/wrong-password") {
-          response.current.innerHTML = "Mot de passe erroné."
-        } else if (error.code === "auth/user-not-found") {
-          response.current.innerHTML = "Utilisateur non trouvé."
-        } else if (error.code === "auth/invalid-email") {
-          response.current.innerHTML = "Email invalide."
+        if (error.code === 'auth/wrong-password') {
+          response.current.innerHTML = 'Mot de passe erroné.'
+        } else if (error.code === 'auth/user-not-found') {
+          response.current.innerHTML = 'Utilisateur non trouvé.'
+        } else if (error.code === 'auth/invalid-email') {
+          response.current.innerHTML = 'Email invalide.'
         } else {
           response.current.innerHTML = error.message
         }
-        response.current.style.color = "var(--color-primary)"
+        response.current.style.color = 'var(--color-primary)'
         const errorCode = error.code
         const errorMessage = error.message
         console.error(errorCode, errorMessage)
@@ -44,7 +44,7 @@ export default function Login() {
         <form
           onSubmit={(e) => {
             e.preventDefault()
-            response.current.innerHTML = "Chargement..."
+            response.current.innerHTML = 'Chargement...'
             Loguser(emailInput.current.value, passInput.current.value)
           }}
         >
@@ -69,7 +69,27 @@ export default function Login() {
               minLength="6"
               ref={passInput}
             />
+
             <label htmlFor="password">Mot de passe*</label>
+            <svg
+              width="24"
+              height="18"
+              viewBox="0 0 24 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              onClick={() => {
+                passInput.current.type === 'password'
+                  ? (passInput.current.type = 'text')
+                  : (passInput.current.type = 'password')
+              }}
+            >
+              <path d="M1 9C1 9 5 1 12 1C19 1 23 9 23 9C23 9 19 17 12 17C5 17 1 9 1 9Z" />
+              <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" />
+            </svg>
           </div>
           <span ref={response}>Les champs marqués * sont obligatoires</span>
           <Btn type="submit">Je me connecte</Btn>
