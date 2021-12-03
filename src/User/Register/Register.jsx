36acb5de7b955a1d0cd64d8341b1ad5c468cd70d
FlanB <clement.renou@mmibordeaux.com>
2021-12-03 15:06:55 +0100

    ⚡️ more p line-height

--- src/User/Register/Register.jsx
+++ src/User/Register/Register.jsx
@@ -68,7 +68,7 @@ export default function Register() {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
               stroke="currentColor"
-              stroke-width="1"
+              stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
               onClick={() => {
