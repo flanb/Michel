70ee66bd398ce32ac4999fa4dc62ebcf30ef8e5a
FlanB <clement.renou@mmibordeaux.com>
2021-12-10 01:19:56 +0100

    ✨ can add a display name on account

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -5,6 +5,7 @@ import { useContext, useEffect } from "react"
 import { fireContext } from "../../App"
 import { signOut } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
+import { updateProfile } from "firebase/auth"
 
 export default function User() {
   const { cookies, setCookie, auth } = useContext(fireContext)
@@ -16,14 +17,44 @@ export default function User() {
     }
   }, [cookies.user, navigate])
 
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    updateProfile(auth.currentUser, {
+      displayName: e.target[0].value,
+    })
+      .then(() => {
+        console.log(cookies.user)
+        setCookie("user", {
+          ...cookies.user,
+          displayName: e.target[0].value,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
     <>
       <div className="user">
         {cookies.user ? (
           <>
             <h1>Profil</h1>
-            <p>Connecté en tant que {cookies.user.email}</p>
-
+            <p>
+              Connecté en tant que{" "}
+              {cookies.user.displayName 
+                ? cookies.user.displayName
+                : cookies.user.email}
+            </p>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input
+                type="text"
+                placeholder="Pseudo"
+                defaultValue={cookies.user.displayName}
+              />
+              <Btn type="submit">Valider</Btn>
+            </form>
             <Btn
               onClick={() => {
                 signOut(auth)
